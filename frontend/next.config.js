2e56715d338e7945773d6cfdcae364548649f6e6
ChandraMoon88 <chandrashekarkuncham7@gmail.com>
2026-01-19 17:35:33 +0530

    Auto-commit [1 files]: frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['shared'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
