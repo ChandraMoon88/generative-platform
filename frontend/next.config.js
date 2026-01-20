5f9c52316580ce3656557f10f9e50dd1794619ae
ChandraMoon88 <chandrashekarkuncham7@gmail.com>
2026-01-20 13:23:38 +0530

    Auto-commit [1 files]: frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['shared'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // We already type-checked during build
+    ignoreBuildErrors: false,
+  },
   async headers() {
     return [
       {
